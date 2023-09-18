13832db4c8533f420fd57fbafc7e604fdd534da1
faithwest <faithchepkoech425@gmail.com>
2023-09-18 20:57:09 +0300

    control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if (x <= 400) {
+    return 'This one is on me!';
+  } else if (x > 2000 && x <= 2500){
+  return 'I will gladly take your thirty bucks.';
+}else if (x > 2500){
+return 'No can do.'
+}else {
+}
+}
+function ternaryCheckCity(b){
+  // Write your code here!
+  if (b === 'NYC'){
+    return 'Ok, sounds good.';
+  }else {
+  return 'No go.';
+}
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(d){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+if (d === 'generous') {
+  return 'Thank you so much.';
+  
+} else if (d === 'not as generous'){
+  return 'Thank you.'
+  } else {
+    return 'Bye.';
+  }
+};
